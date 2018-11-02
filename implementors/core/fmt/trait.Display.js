7b62726905ee573a6c3a496f22c9a8312391bcef
Travis CI User <travis@example.org>
2018-11-02 21:56:14 +0000

    Documentation for alexcrichton/filetime@65c6af3

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["filetime"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"filetime/struct.FileTime.html\" title=\"struct filetime::FileTime\">FileTime</a>",synthetic:false,types:["filetime::FileTime"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["filetime"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"filetime/struct.FileTime.html\" title=\"struct filetime::FileTime\">FileTime</a>",synthetic:false,types:["filetime::FileTime"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
